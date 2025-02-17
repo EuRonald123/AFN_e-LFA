20d4235d036000212a8c0db886220c3ae1871c44
EuRonald123 <ronalds4d123@gmail.com>
2025-02-16 22:11:25 -0300

    versao antiga

---
+++ Main.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ESTADOS 5
+#define MAX_TRANSICOES 7
+
+typedef struct {
+    int de;
+    char simbolo;
+    int para;
+} Transicao;
+
+typedef struct {
+    int numEstados;
+    int numTransicoes;
+    int estadoInicial;
+    int estadosFinais[MAX_ESTADOS];
+    Transicao transicoes[MAX_TRANSICOES];
+} AF_e;
+
+void fechoEpsilon(int estado, int fecho[], AF_e *af) {
+    fecho[estado] = 1;
+    for (int i = 0; i < af->numTransicoes; i++) {
+        if (af->transicoes[i].de == estado && af->transicoes[i].simbolo == '&' && !fecho[af->transicoes[i].para]) {
+            fechoEpsilon(af->transicoes[i].para, fecho, af);
+        }
+    }
+}
+
+void imprimirFechoEpsilon(AF_e *af) {
+    for (int estado = 0; estado < af->numEstados; estado++) {
+        int fecho[MAX_ESTADOS] = {0};
+        fechoEpsilon(estado, fecho, af);
+        printf("Fecho epsilon do estado %d: {", estado);
+        int primeiro = 1;
+        for (int i = 0; i < af->numEstados; i++) {
+            if (fecho[i]) {
+                if (!primeiro) {
+                    printf(",");
+                }
+                printf("%d", i);
+                primeiro = 0;
+            }
+        }
+        printf("}\n");
+    }
+}
+
+int ehAceito(AF_e *af, char *palavra) {
+    int estadosAtuais[MAX_ESTADOS] = {0};
+    int proximosEstados[MAX_ESTADOS] = {0};
+    int fecho[MAX_ESTADOS] = {0};
+
+    fechoEpsilon(af->estadoInicial, fecho, af);
+    for (int i = 0; i < af->numEstados; i++) {
+        if (fecho[i]) {
+            estadosAtuais[i] = 1;
+        }
+    }
+
+    for (int i = 0; palavra[i] != '\0'; i++) {
+        char simbolo = palavra[i];
+        memset(proximosEstados, 0, sizeof(proximosEstados));
+        for (int j = 0; j < af->numEstados; j++) {
+            if (estadosAtuais[j]) {
+                for (int k = 0; k < af->numTransicoes; k++) {
+                    if (af->transicoes[k].de == j && af->transicoes[k].simbolo == simbolo) {
+                        int fechoTemp[MAX_ESTADOS] = {0};
+                        fechoEpsilon(af->transicoes[k].para, fechoTemp, af);
+                        for (int l = 0; l < af->numEstados; l++) {
+                            if (fechoTemp[l]) {
+                                proximosEstados[l] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        memcpy(estadosAtuais, proximosEstados, sizeof(estadosAtuais));
+        // Recalcular o fecho epsilon para os estados atuais
+        memset(fecho, 0, sizeof(fecho));
+        for (int j = 0; j < af->numEstados; j++) {
+            if (estadosAtuais[j]) {
+                fechoEpsilon(j, fecho, af);
+            }
+        }
+        memcpy(estadosAtuais, fecho, sizeof(estadosAtuais));
+    }
+
+    for (int i = 0; i < af->numEstados; i++) {
+        if (estadosAtuais[i]) {
+            for (int j = 0; j < MAX_ESTADOS; j++) {
+                if (af->estadosFinais[j] == i) {
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main(void) {
+    AF_e af;
+    FILE *file = fopen("automato.txt", "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    fscanf(file, "%d", &af.numEstados);
+    fscanf(file, "%d", &af.estadoInicial);
+    fscanf(file, "%d", &af.numTransicoes);
+
+    for (int i = 0; i < MAX_ESTADOS; i++) {
+        fscanf(file, "%d", &af.estadosFinais[i]);
+        if (af.estadosFinais[i] == -1) {
+            break;
+        }
+    }
+
+    for (int i = 0; i < af.numTransicoes; i++) {
+        int de, para;
+        char simbolo;
+        fscanf(file, "%d %c %d", &de, &simbolo, &para);
+        af.transicoes[i] = (Transicao){de, simbolo, para};
+    }
+
+    fclose(file);
+    int rodando = 1;
+
+    while (rodando) {
+        printf("\n1-Imprimir fecho epsilon\n2-Verificar palavra\n3-Sair\n");
+        int opcao;
+        scanf("%d", &opcao);
+        switch (opcao) {
+            case 1:
+                imprimirFechoEpsilon(&af);
+                break;
+            case 2: {
+                char palavra[100];
+                printf("\nDigite uma palavra: ");
+                scanf("%s", palavra);
+
+                if (ehAceito(&af, palavra)) {
+                    printf("A palavra eh aceita.\n");
+                } else {
+                    printf("A palavra nao eh aceita.\n");
+                }
+                break;
+            }
+            case 3:
+                rodando = 0;
+                break;
+            default:
+                printf("Opcao invalida\n");
+                break;
+        }
+    }
+
+    printf("Pressione qualquer tecla para fechar o programa...\n");
+    getchar();
+    getchar();
+    return 0;
+}
